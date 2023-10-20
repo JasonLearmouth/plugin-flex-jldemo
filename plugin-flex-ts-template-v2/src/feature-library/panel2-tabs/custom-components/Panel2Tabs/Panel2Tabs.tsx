955381ca985ab2631eb0530a50e05926310ae6f0
Christopher Connolly <425070+chaosloth@users.noreply.github.com>
2023-10-20 15:25:03 +1100

    Add conversation summary

--- plugin-flex-ts-template-v2/src/feature-library/panel2-tabs/custom-components/Panel2Tabs/Panel2Tabs.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/panel2-tabs/custom-components/Panel2Tabs/Panel2Tabs.tsx
@@ -4,33 +4,37 @@ import CannedResponsesCRM from '../CannedResponses/CannedResponsesCRM';
 import ConversationCardsCRM from '../AdaptiveCards/ConversationCardsCRM';
 import Knowledge from '../Knowledge/Knowledge';
 import SegmentView from '../Segment/SegmentView';
+import ConversationSummaryCard from '../ConversationSummary/ConversationSummaryCard';
 
 const Panel2Tabs = () => {
   const selectedId = useUID();
 
   return (
-    <Tabs selectedId={selectedId} baseId="panel2-fitted-tabs" variant="fitted">
-      <TabList aria-label="Panel 2 tabs">
-        <Tab id={selectedId}>Profile</Tab>
-        <Tab>Cards</Tab>
-        <Tab>Responses</Tab>
-        <Tab>Knowledge</Tab>
-      </TabList>
-      <TabPanels>
-        <TabPanel>
-          <SegmentView />
-        </TabPanel>
-        <TabPanel>
-          <ConversationCardsCRM />
-        </TabPanel>
-        <TabPanel>
-          <CannedResponsesCRM />
-        </TabPanel>
-        <TabPanel>
-          <Knowledge />
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+    <>
+      <ConversationSummaryCard currentLanguage={'en'} />
+      <Tabs selectedId={selectedId} baseId="panel2-fitted-tabs" variant="fitted">
+        <TabList aria-label="Panel 2 tabs">
+          <Tab id={selectedId}>Profile</Tab>
+          <Tab>Cards</Tab>
+          <Tab>Responses</Tab>
+          <Tab>Knowledge</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <SegmentView />
+          </TabPanel>
+          <TabPanel>
+            <ConversationCardsCRM />
+          </TabPanel>
+          <TabPanel>
+            <CannedResponsesCRM />
+          </TabPanel>
+          <TabPanel>
+            <Knowledge />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
   );
 };
 
---
+++ plugin-flex-ts-template-v2/src/feature-library/panel2-tabs/types/CustomerType.ts
@@ -0,0 +1,6 @@
+export type CustomerType = {
+  external_id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+};
