cdba43a9a69ace39c5f1a6b28508819c2813f9cb
Christopher Connolly <425070+chaosloth@users.noreply.github.com>
2023-10-20 15:25:41 +1100

    Add video invite card

--- plugin-flex-ts-template-v2/src/feature-library/panel2-tabs/flex-hooks/components/SegmentActions.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/panel2-tabs/flex-hooks/components/SegmentActions.tsx
@@ -1,34 +1,52 @@
-// import * as Flex from '@twilio/flex-ui';
+import * as Flex from '@twilio/flex-ui';
 
-// import { FlexComponent } from '../../../../types/feature-loader';
-// import { WorkerAcceptTaskActionPayload } from '@twilio/flex-ui/src/actions/WorkerActions';
-// import SegmentService from '../../utils/SegmentService/SegmentService';
+import { FlexComponent } from '../../../../types/feature-loader';
+import { WorkerAcceptTaskActionPayload } from '@twilio/flex-ui/src/actions/WorkerActions';
+import SegmentService from '../../utils/SegmentService/SegmentService';
+import PushSummary from 'feature-library/panel2-tabs/utils/PushSummary';
 
-// export const componentName = FlexComponent.CRMContainer;
-// export const componentHook = function addSegmentActions(flex: typeof Flex, _manager: Flex.Manager) {
-//   flex.Actions.addListener('beforeAcceptTask', async (payload: WorkerAcceptTaskActionPayload) => {
-//     if (payload.task?.attributes.email) {
-//       await SegmentService.sendToSegment({
-//         actor: 'Flex',
-//         eventName: 'Agent Accept',
-//         userId: payload.task?.attributes.email,
-//         ConversationSid: payload.task?.attributes.conversationSid,
-//         direction: payload.task?.attributes.direction,
-//         channelType: payload.task?.attributes.channelType,
-//       });
-//     }
-//   });
+export const componentName = FlexComponent.CRMContainer;
+export const componentHook = function addSegmentActions(flex: typeof Flex, _manager: Flex.Manager) {
+  //   flex.Actions.addListener('beforeAcceptTask', async (payload: WorkerAcceptTaskActionPayload) => {
+  //     if (payload.task?.attributes.email) {
+  //       await SegmentService.sendToSegment({
+  //         actor: 'Flex',
+  //         eventName: 'Agent Accept',
+  //         userId: payload.task?.attributes.email,
+  //         ConversationSid: payload.task?.attributes.conversationSid,
+  //         direction: payload.task?.attributes.direction,
+  //         channelType: payload.task?.attributes.channelType,
+  //       });
+  //     }
+  //   });
 
-//   flex.Actions.addListener('beforeCompleteTask', async (payload: WorkerAcceptTaskActionPayload) => {
-//     if (payload.task?.attributes.email) {
-//       await SegmentService.sendToSegment({
-//         actor: 'Flex',
-//         eventName: 'Agent Complete',
-//         userId: payload.task?.attributes.email,
-//         ConversationSid: payload.task?.attributes.conversationSid,
-//         direction: payload.task?.attributes.direction,
-//         channelType: payload.task?.attributes.channelType,
-//       });
-//     }
-//   });
-// };
+  flex.Actions.addListener('beforeCompleteTask', async (payload: WorkerAcceptTaskActionPayload) => {
+    // if (payload.task?.attributes.email) {
+    //   await SegmentService.sendToSegment({
+    //     actor: 'Flex',
+    //     eventName: 'Agent Complete',
+    //     userId: payload.task?.attributes.email,
+    //     ConversationSid: payload.task?.attributes.conversationSid,
+    //     direction: payload.task?.attributes.direction,
+    //     channelType: payload.task?.attributes.channelType,
+    //   });
+    // }
+    // ***BROKEN - NOT YET COMPLETED CODING
+    // try {
+    //     if (!payload.task?.attributes.customer?.id || !payload.task?.attributes.customer?.email || !summaryRaw) {
+    //       throw new Error('Missing properties');
+    //     }
+    //     const summary = typeof summaryRaw === 'string' ? summaryRaw?.replace(/^\n+/, '') : summaryRaw;
+    //     await PushSummary.push(
+    //         payload.task?.attributes.customer?.id as string,
+    //         payload.task?.attributes.customer?.email as string,
+    //         summary,
+    //       sentiment?.sentiment as string,
+    //       payload.task?.attributes.currentLanguage,
+    //     );
+    //   } catch (error: any) {
+    //     const err = error.message;
+    //     console.error(err);
+    //   }
+  });
+};
